13a4718c827222b745e26eecc0b069eff6cab978
wuslp <wuslp@naver.com>
2021-12-19 23:45:59 +0900

    YoilTeller (target 터미널 이용 출력 안됨)

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -15,12 +15,12 @@ public class YoilTeller {
 		int mm = Integer.parseInt(month);
 		int dd = Integer.parseInt(day);
 		
-		// 2. 작업
+		// 2. 작업 (날짜 세팅)
 		Calendar cal = Calendar.getInstance();
 		cal.set(yyyy, mm -1,dd);
 		
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-		char yoil = "일월화수목금토".charAt(dayOfWeek);
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1:일요일, 2: 월요일 
+		char yoil = "일월화수목금토".charAt(dayOfWeek); 
 		
 		// 3. 출력
 		System.out.println(year+"년 "+month+"월 "+day+"일은 ");
