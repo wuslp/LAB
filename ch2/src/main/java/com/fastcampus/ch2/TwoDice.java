9d72d33eb1e2ca57ca7a2e0cebc0349eae9289ad
wuslp <wuslp@naver.com>
2021-12-21 01:30:14 +0900

    ch2.02-04 HTTP 요청과 응답 예제 (브라우저 출력 기본 형태)

---
+++ ch2/src/main/java/com/fastcampus/ch2/TwoDice.java
@@ -0,0 +1,29 @@
+package com.fastcampus.ch2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // 1. 원격 호출 가능한 프로그램으로 등록 
+public class TwoDice {
+	
+	@RequestMapping("/rollDice") // 2. url과 메서드 연결 
+	
+	// 3. 브라우저로 출력하기 위해 HttpServletResponse response 객체 필요
+	public void main(HttpServletResponse response)throws IOException { 
+		//4.
+		response.setContentType("text/html");	
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>"); 
+		out.println("<head>"); 
+		out.println("</head>"); 
+		out.println("<body>"); 
+		out.println("</body>"); 
+		out.println("</html>"); 
+	}
+}
